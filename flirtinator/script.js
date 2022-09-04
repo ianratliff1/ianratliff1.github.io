adbd6a4eeb793c196f02168f37b79d5d47ef89cc
ianratliff1 <ratlifim@gmail.com>
2022-09-04 09:48:07 +0200

    Flirtinator: optimized script.js, no operational or visual changes

--- flirtinator/script.js
+++ flirtinator/script.js
@@ -1,3 +1,4 @@
+console.log("Why howdy there, stranger! Feel free to check out my GitHub at www.github.com/iratliff0")
 var generateButton = document.getElementById('generate')
 var messageBox = document.getElementById('flirtyMessage')
 
@@ -12,13 +13,23 @@ function flirt() {
     const noun = ['you sexy thang', 'good lookin\'','you absolute specimen','baby','darlin\'','sweetie','bella']
     const action = ['grab a drink','meet for coffee','get to know each other better','go on a date','go eat samples at Costco','go out for a bello apperitivo']
     const timeframe = ['after work','tomorrow','this weekend','Friday night','after church','after school']
-    const greetingIndex = Math.floor(Math.random()*greeting.length)
-    const nounIndex = Math.floor(Math.random()*noun.length)
-    const actionIndex = Math.floor(Math.random()*timeframe.length)
-    const timeframeIndex = Math.floor(Math.random()*action.length)
+    // const greetingIndex = Math.floor(Math.random()*greeting.length)
+    // const nounIndex = Math.floor(Math.random()*noun.length)
+    // const actionIndex = Math.floor(Math.random()*timeframe.length)
+    // const timeframeIndex = Math.floor(Math.random()*action.length)
+    const thisGreeting = randomize(greeting)
+    const thisNoun = randomize(noun)
+    const thisAction = randomize(action)
+    const thisTimeframe = randomize (action)
+    
     // messageBox.innerHTML=`${greetingIndex} ${nounIndex} ${actionIndex} ${timeframeIndex}`
     // messageBox.innerHTML=greeting.length
-    const flirtyMessage = `<h2>${greeting[greetingIndex]} ${noun[nounIndex]}, would you like to ${action[actionIndex]} ${timeframe[timeframeIndex]}?</h2>`
+    const flirtyMessage = `<h2>${thisGreeting} ${thisNoun}, would you like to ${thisAction} ${thisTimeframe}?</h2>`
     messageBox.innerHTML=flirtyMessage
+}
+
+function randomize(rArray) {
+  const randomized = rArray[Math.floor(Math.random()*rArray.length)]
+  return randomized
 }
 
